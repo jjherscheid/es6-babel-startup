5924a5ce42e8fa2f54c57f6897caaae88bdfd3b0
Johan Herscheid <Johan.Herscheid@ictu.nl>
2016-05-25 14:22:10 +0200

    Empty Dog class

--- src/dog.js
+++ src/dog.js
@@ -1,21 +1,4 @@
 // Sample class for showing that Babel works
 
-class Dog {
-	constructor(defaultBarkStyle) {
-		this._barkStyle = defaultBarkStyle;
-	}
-	
-	get barkStyle() {
-		return this._barkStyle;
-	}
-	set barkStyle(value) {
-		this._barkStyle = value;
-	}
-
-	bark() {
-		console.log(this.barkStyle);		
-	}
+class Dog {	
 }
-
-var doggy = new Dog('Woef');
-doggy.bark();
